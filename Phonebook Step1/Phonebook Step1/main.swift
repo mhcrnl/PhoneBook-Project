2fe47eeb9eb767e6d9b790e99e19cb84ad18f02c
juyonglee <juyonglee0208@gmail.com>
2017-10-13 18:26:44 +0900

    PhoneInfo Class 생성

---
+++ Phonebook Step1/Phonebook Step1/main.swift
@@ -0,0 +1,34 @@
+//
+//  main.swift
+//  Phonebook Step1
+//
+//  Created by JuYong Lee.
+//  Copyright © 2017년 JuYong Lee. All rights reserved.
+//
+
+import Foundation
+
+class PhoneInfo {
+    var name:String
+    var phoneNumber:String
+    var birthday:String?
+    
+    init(name:String, phoneNumber:String, birthDay:String?) {
+        self.name = name
+        self.phoneNumber = phoneNumber
+        self.birthday = birthDay
+    }
+    
+    convenience init(name:String, phoneNumber:String) {
+        self.init(name: name, phoneNumber: phoneNumber)
+    }
+    
+    func printPhoneInfo() -> Void {
+        print("Name: \(self.name)")
+        print("Phone Number: \(self.phoneNumber)")
+        if let birthDay = self.birthday {
+            print("Birthday: \(birthDay)")
+        }
+    }
+}
+
