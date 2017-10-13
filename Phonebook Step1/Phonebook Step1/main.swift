aa56ad9a4b8e29ab4be5f185e8c417ebbff07685
juyonglee <juyonglee0208@gmail.com>
2017-10-13 18:51:59 +0900

    코드에 대한 주석 추가 및 정리

--- Phonebook Step1/Phonebook Step1/main.swift
+++ Phonebook Step1/Phonebook Step1/main.swift
@@ -8,9 +8,14 @@
 
 import Foundation
 
+//  Designated Initializer를 이용하여 객체 생성
 let person01 = PhoneInfo(name: "Juyong Lee", phoneNumber: "010-****-****", birthDay: "1990-02-08")
+let person02 = PhoneInfo(name: "Lukaku", phoneNumber: "010-****-****", birthDay: nil)
+
+//  Convenience Initializer를 이용하여 객체 생성
+let person03 = PhoneInfo(name: "C.Ronaldo", phoneNumber: "010-****-****")
+
+//  객체의 연락처 정보를 출력
 person01.printPhoneInfo()
-let person02 = PhoneInfo(name: "C.Ronaldo", phoneNumber: "010-****-****")
 person02.printPhoneInfo()
-let person03 = PhoneInfo(name: "Lukaku", phoneNumber: "010-****-****", birthDay: nil)
 person03.printPhoneInfo()
