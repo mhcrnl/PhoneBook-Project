342d835190da5fec85b72b4ab8ac6dcb5dd9a5cf
juyonglee <juyonglee0208@gmail.com>
2018-01-03 23:23:37 +0900

    PhoneCompanyInfo Class 작성

--- Phonebook Step4/Phonebook Step4/PhoneCompanyInfo.swift
+++ Phonebook Step4/Phonebook Step4/PhoneCompanyInfo.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+class PhoneCompanyInfo {
+    //  Phonebook에 필요한 property 선언
+    var name:String
+    var phoneNumber:String
+    var company:String
+    
+    //  Designated Initializer 정의
+    init(name:String, phoneNumber:String, company:String) {
+        self.name = name
+        self.phoneNumber = phoneNumber
+        self.company = company
+    }
+    
+    //  객체의 저장된 property를 출력하는 기능 정의
+    func printPhoneInfo() -> Void {
+        print("\n<입력된 정보 출력>")
+        print("Name: \(self.name)")
+        print("Phone Number: \(self.phoneNumber)")
+        print("Company: \(self.company)")
+    }
+}
